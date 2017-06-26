87a9a5f6c64fed49b85cb576187d1821a1f3ffbb
melkiga <melkiga@vcu.edu>
2017-06-26 12:30:35 -0400

    Modified the post-hoc script
    1. account for OS + paths
    2. use tables instead of reading from xlsx

--- post_hoc_script.m
+++ post_hoc_script.m
@@ -1,49 +1,45 @@
 clear; clc; close all;
 
-filename = 'Results.xlsx';
-path_in = 'C:\Users\melkiga\Google Drive\Research\Cano\MultiInstanceClassification\WekaTesting\output\';
-path_out = 'C:\Users\melkiga\Google Drive\MatlabHelpers\stats_tables\';
-file_path = [path_in filename];
-metrics = {'accuracy','precision','recall','kappa','auc','Time'};
+if ispc
+    disp('TODO: add paths for windows machine.');
+    disp('Terminating script.'); return;
+else
+    path_in = '/Users/gabriellamelki/Documents/Research/MultiInstanceLearning/WekaTesting/output/';
+    path_out = '/Users/gabriellamelki/Documents/Research/MultiInstanceLearning/WekaTesting/stats/';
+end
+
+metrics = {'Accuracy','Precision','Recall','Kappa','AUC','Time'};
 num_metrics = length(metrics);
 alpha = 0.1;
-
-% Get algorithm and datasets names and lengths
-[~, ~, prints] = xlsread(file_path,2,'A1:L1');
-prints(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),prints)) = {NaN};
-algorithms = prints(2:end);
+% initialize using Accuracy.csv
+T = readtable([path_in metrics{1} '.csv']);
+% get algorithm names
+algorithms = T.Properties.VariableNames(2:end);
 num_algorithms = length(algorithms);
-
-[~, ~, datasets] = xlsread(file_path,2,'A2:A16');
-datasets(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),datasets)) = {NaN};
-datasets = datasets';
+% get dataset names + latex table row names
+datasets = table2cell(T(:,1));
 num_datasets = length(datasets);
-latex_data_rows = datasets; latex_data_rows{end+1} = 'Average'; latex_data_rows{end+1} = 'Rank';
-
-% Allocate memory
+latex_data_rows = datasets; latex_data_rows{end+1} = 'Average'; latex_data_rows{end+1} = 'Rank'; 
 ranks = zeros(num_metrics,num_algorithms);
 
-for m = 1:length(metrics)
-    metric = metrics{m};
-    disp(metric);
-    
-    [~, ~, data] = xlsread(file_path,metric,'B2:L16');
-    data(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),data)) = {NaN};
-    data = cell2mat(data);
-    
+for m = 1:num_metrics
+    % get data
+    T = readtable([path_in metrics{m} '.csv']);
+    data = T{:,2:end};
+    disp(['Analyzing ' metrics{m}]);
     [avg_ranks,chi_statistics] = avg_rank(-data);
     ranks(m,:) = avg_ranks;
-    %max_rank = round(max(avg_ranks))+1;
-    %[stats,headers] = wilcoxon(data,alpha);
+    max_rank = round(max(avg_ranks))+1;
+    [stats,headers] = wilcoxon(data,alpha);
     
-    %data(end+1,:) = mean(data);
-    %data(end+1,:) = avg_ranks;
+    % add average + ranks to data
+    data(end+1,:) = mean(data);
+    data(end+1,:) = avg_ranks;
     
-    %matrix2latex_metrics(data, [path_out [metric '.tex']], 'rowLabels', latex_data_rows, 'columnLabels', algorithms, 'alignment', 'c', 'format', '%-1.1f', 'rank', 'yes');
-    % create results table latex
-    %matrix2latex(stats, [path_out [metric '_wx.tex']], 'rowLabels', algorithms, 'columnLabels', headers, 'alignment', 'c', 'format', '%-1.4f');
-    
-    % create Bonferroni-Dunn CD figure
-    %bonferroni_dunn_tikz([path_out [metric '_Fig.tex']],avg_ranks,1,num_algorithms,algorithms,max_rank);
-    
+    % create latex table
+    matrix2latex_metrics(data, [path_out [metric '.tex']], 'rowLabels', latex_data_rows, 'columnLabels', algorithms, 'alignment', 'c', 'format', '%-1.1f', 'rank', 'yes');
+    % create results table
+    matrix2latex(stats, [path_out [metric '_wx.tex']], 'rowLabels', algorithms, 'columnLabels', headers, 'alignment', 'c', 'format', '%-1.4f');
+    % create bonferroni-dunn figure
+    bonferroni_dunn_tikz([path_out [metric '_Fig.tex']],avg_ranks,1,num_algorithms,algorithms,max_rank);
 end
